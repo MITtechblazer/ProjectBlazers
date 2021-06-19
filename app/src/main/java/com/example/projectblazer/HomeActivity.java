af3f2d8da556b7ec8c78ab43f45cd8c6a496c2fb
Sagar Sahore <ssagarsahore11@gmail.com>
2021-06-20 05:49:32 +1200

    Google Auth

---
+++ app/src/main/java/com/example/projectblazer/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.projectblazer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView name,email;
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        logout = findViewById(R.id.Logout);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount !=null){
+            name.setText(signInAccount.getDisplayName());
+            email.setText(signInAccount.getEmail());
+        }
+
+        logout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
